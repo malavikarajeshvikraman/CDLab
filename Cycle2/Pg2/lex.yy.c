8e6394ca489ed3741f5dff3b99e9daeb51c0eece
malavikarajeshvikraman <malu.rajesh2000@gmail.com>
2021-11-07 09:38:02 +0530

    Update Program 2

--- Cycle2/Pg2/lex.yy.c
+++ Cycle2/Pg2/lex.yy.c
@@ -284,28 +284,27 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 
 #define YY_NUM_RULES 2
 #define YY_END_OF_BUFFER 3
-static yyconst short int yy_accept[19] =
+static yyconst short int yy_accept[8] =
     {   0,
-        0,    0,    3,    2,    2,    2,    0,    0,    0,    0,
-        0,    0,    1,    0,    0,    0,    0,    0
+        1,    1,    3,    2,    1,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    3,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    4,    1,    1,    1,
+        1,    1,    1,    1,    2,    2,    2,    2,    2,    2,
+        2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
+        2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
+        1,    1,    1,    1,    1,    1,    2,    2,    2,    2,
 
-        1,    1,    1,    1,    1,    1,    1,    5,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
+        2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
+        2,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -322,37 +321,29 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[6] =
+static yyconst int yy_meta[3] =
     {   0,
-        1,    2,    1,    1,    1
+        1,    2
     } ;
 
-static yyconst short int yy_base[22] =
+static yyconst short int yy_base[9] =
     {   0,
-        0,    2,   23,   19,   24,    3,   18,    0,   15,   15,
-        6,    8,   24,   14,   12,   12,    0,   24,   11,    0,
-       13
+        0,    0,    4,    5,    0,    0,    5,    1
     } ;
 
-static yyconst short int yy_def[22] =
+static yyconst short int yy_def[9] =
     {   0,
-       19,   19,   18,   20,   18,   20,   20,    6,    7,    7,
-       21,   21,   18,   12,   12,   12,   12,    0,   18,   18,
-       18
+        7,    1,    7,    7,    8,    8,    0,    7
     } ;
 
-static yyconst short int yy_nxt[30] =
+static yyconst short int yy_nxt[8] =
     {   0,
-        7,    5,    6,    5,    6,    8,    9,   13,   14,   13,
-       14,    4,    4,   12,   12,   17,   16,   15,   11,   10,
-        8,    8,   18,    3,   18,   18,   18,   18,   18
+        4,    5,    6,    7,    3,    7,    7
     } ;
 
-static yyconst short int yy_chk[30] =
+static yyconst short int yy_chk[8] =
     {   0,
-       20,    1,    1,    2,    2,    6,    6,   11,   11,   12,
-       12,   19,   19,   21,   21,   16,   15,   14,   10,    9,
-        7,    4,    3,   18,   18,   18,   18,   18,   18
+        1,    1,    8,    3,    7,    7,    7
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -369,8 +360,14 @@ char *yytext;
 #line 1 "pg1.l"
 #define INITIAL 0
 #line 2 "pg1.l"
-int flag = 0;
-#line 374 "lex.yy.c"
+
+#include<stdio.h>
+
+#include<string.h>
+
+int i;
+
+#line 371 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -521,9 +518,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 5 "pg1.l"
+#line 12 "pg1.l"
 
-#line 527 "lex.yy.c"
+#line 524 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -574,13 +571,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 19 )
+				if ( yy_current_state >= 8 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 24 );
+		while ( yy_base[yy_current_state] != 5 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -608,15 +605,39 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "pg1.l"
-flag=1;
+#line 13 "pg1.l"
+{
+
+for(i=0;i<=yyleng;i++)
+
+{
+
+if((yytext[i]=='m')&&(yytext[i+1]=='a')&&(yytext[i+2]=='l')&&(yytext[i+3]=='a'))
+
+{
+
+printf("%s","Not Accepted");
+break;
+
+}
+
+if(i ==yyleng-1)
+{
+    printf("%s","Accepted");
+}
+
+}
+
+
+
+}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 7 "pg1.l"
+#line 39 "pg1.l"
 ECHO;
 	YY_BREAK
-#line 620 "lex.yy.c"
+#line 641 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -908,7 +929,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 19 )
+			if ( yy_current_state >= 8 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -943,11 +964,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 19 )
+		if ( yy_current_state >= 8 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 18);
+	yy_is_jam = (yy_current_state == 7);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1502,16 +1523,23 @@ int main()
 	return 0;
 	}
 #endif
-#line 7 "pg1.l"
+#line 39 "pg1.l"
 
  
-int yywrap(void) {}
  
-main(){
-    yylex();
-     if (flag == 1) 
-    printf("Accepted"); 
- else
-    printf("Not Accepted"); 
-    return 0;
+
+main()
+
+{
+
+yylex();
+
+}
+
+int yywrap()
+
+{
+
+return 1;
+
 }
